6d423adbd8312ba1cb8578eba6c6a78bcb7d6296
inatatsu <tatsuhiro.9699@gmail.com>
2023-05-30 16:49:54 +0900

    :construction: wip

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -12,11 +12,12 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Typography,
 } from '@mui/material';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import { SSSWindow } from 'sss-module';
-import { Address, RepositoryFactoryHttp, TransactionGroup } from 'symbol-sdk';
+import { Address, RepositoryFactoryHttp } from 'symbol-sdk';
 declare const window: SSSWindow;
 
 type ApostilleInfo = {
@@ -36,53 +37,72 @@ function MyPage(): JSX.Element {
     const f = async () => {
       const NODE = await connectNode(nodeList);
       if (NODE === '') return undefined;
-      const repo = new RepositoryFactoryHttp(NODE, {
-        websocketUrl: NODE.replace('http', 'ws') + '/ws',
-        websocketInjected: WebSocket,
-      });
-      const multisigRepo = repo.createMultisigRepository();
-      const accountRepo = repo.createAccountRepository();
-      const metadataRepo = repo.createMetadataRepository();
-      const info = await multisigRepo
-        .getMultisigAccountInfo(Address.createFromRawAddress(address))
-        .toPromise();
-      const apostilleAddresses = info?.multisigAddresses ?? [];
-
-      const addressPromise = await Promise.all(apostilleAddresses).then((addresses) =>
-        addresses.map((address) => accountRepo.getAccountInfo(address).toPromise())
-      );
-
-      const apostilleAccounts = await Promise.all(addressPromise);
-
-      const accountsPromise = apostilleAccounts.map(async (account) => {
-        const r = await metadataRepo
-          .search({
-            targetAddress: account?.address,
-          })
+      try {
+        const repo = new RepositoryFactoryHttp(NODE, {
+          websocketUrl: NODE.replace('http', 'ws') + '/ws',
+          websocketInjected: WebSocket,
+        });
+        const multisigRepo = repo.createMultisigRepository();
+        const accountRepo = repo.createAccountRepository();
+        const metadataRepo = repo.createMetadataRepository();
+        const info = await multisigRepo
+          .getMultisigAccountInfo(Address.createFromRawAddress(address))
           .toPromise();
-        return r?.data;
-      });
+        const apostilleAddresses = info?.multisigAddresses ?? [];
 
-      const metadata = await Promise.all(accountsPromise);
+        const addressPromise = await Promise.all(apostilleAddresses).then((addresses) =>
+          addresses.map((address) => accountRepo.getAccountInfo(address).toPromise())
+        );
 
-      const infos: ApostilleInfo[] = metadata
-        .map((m) => {
-          if (!!m && m.length !== 0) {
-            const data = {
-              address: m[0].metadataEntry.targetAddress?.plain(),
-              fileName: m
-                .filter((m) => m.metadataEntry.scopedMetadataKey.toHex() === MetadataKey.filename)
-                .map((m) => m.metadataEntry.value)[0],
-            };
-            return data;
-          }
-          return undefined;
-        })
-        .filter((i) => !!i) as ApostilleInfo[];
-      setApostilleInfo(infos);
+        const apostilleAccounts = await Promise.all(addressPromise);
+
+        const accountsPromise = apostilleAccounts.map(async (account) => {
+          const r = await metadataRepo
+            .search({
+              targetAddress: account?.address,
+            })
+            .toPromise();
+          return r?.data;
+        });
+
+        const metadata = await Promise.all(accountsPromise);
+
+        const infos: ApostilleInfo[] = metadata
+          .map((m) => {
+            if (!!m && m.length !== 0) {
+              const data = {
+                address: m[0].metadataEntry.targetAddress?.plain(),
+                fileName: m
+                  .filter((m) => m.metadataEntry.scopedMetadataKey.toHex() === MetadataKey.filename)
+                  .map((m) => m.metadataEntry.value)[0],
+              };
+              return data;
+            }
+            return undefined;
+          })
+          .filter((i) => !!i) as ApostilleInfo[];
+        setApostilleInfo(infos);
+      } catch (e) {
+        console.log(e);
+      }
     };
     f();
   }, []);
+
+  console.log(apostilleInfo);
+
+  if (apostilleInfo.length === 0) {
+    return (
+      <>
+        <Header setOpenLeftDrawer={setOpenLeftDrawer} />
+        <LeftDrawer openLeftDrawer={openLeftDrawer} setOpenLeftDrawer={setOpenLeftDrawer} />
+        <Box display='flex' flexDirection='column' alignItems='center' marginTop='80px'>
+          <Typography>You don't have ApostilleAccount</Typography>
+        </Box>
+      </>
+    );
+  }
+
   return (
     <>
       <Header setOpenLeftDrawer={setOpenLeftDrawer} />
